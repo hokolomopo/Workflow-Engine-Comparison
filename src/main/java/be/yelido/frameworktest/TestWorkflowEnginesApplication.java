3ae5c678de2517f54785e2a925538a14c07eed97
hokolomopo <adrien.minne@outlook.com>
2020-02-27 12:00:12 +0100

    So hard to make work..

--- src/main/java/be/yelido/frameworktest/TestWorkflowEnginesApplication.java
+++ src/main/java/be/yelido/frameworktest/TestWorkflowEnginesApplication.java
@@ -2,6 +2,9 @@ package be.yelido.frameworktest;
 
 import be.yelido.frameworktest.objects.Order;
 import be.yelido.frameworktest.routes.JmsTestRouter;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,12 +15,16 @@ import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
 @EnableJms
-@ImportResource(value= {"classpath:jbpm.xml",})
+//@ImportResource(value= {"classpath:jbpm.xml",})
 public class TestWorkflowEnginesApplication {
 
 	public static void main(String[] args) {
 		// Launch the application
 		ConfigurableApplicationContext context = SpringApplication.run(TestWorkflowEnginesApplication.class, args);
+
+//		KieServices kService = KieServices.Factory.get();
+//		KieContainer kContainer = kService.getKieClasspathContainer();
+//		KieBase kbase = kContainer.getKieBase("id");
 	}
 
 }
--- src/main/java/be/yelido/frameworktest/routes/JmsTestRouter.java
+++ src/main/java/be/yelido/frameworktest/routes/JmsTestRouter.java
@@ -13,21 +13,21 @@ public class JmsTestRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        System.out.println("Configuring route");
-
-        from("jms:{{input.queue}}")
-                .log(LoggingLevel.DEBUG, log, "New message in input queue")
-                .choice()
-                    .when().jsonpath(("$[?(@.clientType =~ /.*VIP/i)]"))
-                        .to("jms:{{shop.queue.vip}}").log(LoggingLevel.DEBUG, log, "Message send to vip shop")
-                    .otherwise().to("jms:{{shop.queue.normal}}").log(LoggingLevel.DEBUG, log, "Message send to normal shop")
-                .end();
-
-        from("jms:{{shop.queue.output}}")
-                .log(LoggingLevel.DEBUG, log, "New message received on shop output queue")
-                .to("jms:{{payment.queue}}")
-                .log(LoggingLevel.DEBUG, log, "Message is successfully sent to the paymeny queue")
-                .end();
+//        System.out.println("Configuring route");
+//
+//        from("jms:{{input.queue}}")
+//                .log(LoggingLevel.DEBUG, log, "New message in input queue")
+//                .choice()
+//                    .when().jsonpath(("$[?(@.clientType =~ /.*VIP/i)]"))
+//                        .to("jms:{{shop.queue.vip}}").log(LoggingLevel.DEBUG, log, "Message send to vip shop")
+//                    .otherwise().to("jms:{{shop.queue.normal}}").log(LoggingLevel.DEBUG, log, "Message send to normal shop")
+//                .end();
+//
+//        from("jms:{{shop.queue.output}}")
+//                .log(LoggingLevel.DEBUG, log, "New message received on shop output queue")
+//                .to("jms:{{payment.queue}}")
+//                .log(LoggingLevel.DEBUG, log, "Message is successfully sent to the paymeny queue")
+//                .end();
 
     }
 }
